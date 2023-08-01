625a7a7857a2aa38d2c6d2c8fd13fbc8ecab1460
Michael Wilson <michael.a.wilson@yale.edu>
2023-08-01 01:42:51 -0400

    api fix

--- core/llama_2/model.py
+++ core/llama_2/model.py
@@ -319,7 +319,7 @@ class Transformer(nn.Module):
         )
 
     @torch.inference_mode()
-    def forward(self, tokens: torch.Tensor, start_pos: int):
+    def forward(self, tokens: torch.Tensor, start_pos: int = 0):
         _bsz, seqlen = tokens.shape
         h = self.tok_embeddings(tokens)
         self.freqs_cis = self.freqs_cis.to(h.device)
